992789a4f69ec1de5278b5aefe3eca00d300c9ad
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2025-04-13 10:36:01 -0400

    Added scripts to deploy K3s to one or more nodes

---
+++ k3s/setup_primary_node.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Ensure both parameters are provided
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <GH_USERNAME> <GH_TOKEN>"
+  exit 1
+fi
+# Assign parameters to variables
+GH_USERNAME=$1
+GH_TOKEN=$2
+
+echo "Updating the system..."
+apt update
+apt upgrade -y
+
+echo "Installing necessary packages..."
+apt install qemu-guest-agent -y
+
+echo "Installing K3s on primary node..."
+curl -sfL https://get.k3s.io | sh -
+
+echo "Creating kubeconfig directory..."
+mkdir -p $HOME/.kube
+chown tsa:tsa $HOME/.kube
+
+echo "Copying K3s kubeconfig to user's kube directory..."
+cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
+
+echo "Setting permissions for kubeconfig..."
+chown tsa:tsa $HOME/.kube/config
+
+echo "Setting KUBECONFIG environment variable..."
+export KUBECONFIG=~/.kube/config
+echo "export KUBECONFIG=$HOME/.kube/config" >> $HOME/.bashrc
+
+echo "Adding GitHub Container Registry configuration..."
+cat <<EOF > /etc/rancher/k3s/registries.yaml
+mirrors:
+  "ghcr.io":
+    endpoint:
+      - "https://ghcr.io"
+configs:
+  "ghcr.io":
+    auth:
+      username: "$GH_USERNAME"
+      password: "$GH_TOKEN"
+EOF
+
+echo "Restarting K3s service..."
+systemctl restart k3s
---
+++ k3s/setup_secondary_node.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Ensure both parameters are provided
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+  echo "Usage: $0 <K3S_URL> <K3S_TOKEN> <GH_USERNAME> <GH_TOKEN>"
+  exit 1
+fi
+
+K3S_URL=$1
+K3S_TOKEN=$2
+GH_USERNAME=$3
+GH_TOKEN=$4
+
+echo "Updating the system..."
+apt update
+apt upgrade -y
+
+echo "Installing necessary packages..."
+apt install qemu-guest-agent -y
+
+echo "Installing K3s on secondary node..."
+curl -sfL https://get.k3s.io | K3S_URL=$K3S_URL K3S_TOKEN=$K3S_TOKEN sh -
+
+echo "Creating kubeconfig directory..."
+mkdir -p $HOME/.kube
+chown tsa:tsa $HOME/.kube
+
+echo "Copying K3s kubeconfig to user's kube directory..."
+cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
+
+echo "Setting permissions for kubeconfig..."
+chown tsa:tsa $HOME/.kube/config
+
+echo "Setting KUBECONFIG environment variable..."
+export KUBECONFIG=~/.kube/config
+echo "export KUBECONFIG=$HOME/.kube/config" >> $HOME/.bashrc
+
+echo "Adding GitHub Container Registry configuration..."
+cat <<EOF > /etc/rancher/k3s/registries.yaml
+mirrors:
+  "ghcr.io":
+    endpoint:
+      - "https://ghcr.io"
+configs:
+  "ghcr.io":
+    auth:
+      username: "$GH_USERNAME"
+      password: "$GH_TOKEN"
+EOF
+
+echo "Restarting K3s service..."
+systemctl restart k3s
