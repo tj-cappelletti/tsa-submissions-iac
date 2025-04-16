fd9e76c6c81c475a51f92bde53abbd5c9e727771
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2025-04-16 11:55:28 +0000

    Created script to deploy Authentik on K3s

---
+++ authentik/k3s/deploy_helm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Ensure both parameters are provided
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <POSTGRESQL_PASSWORD> <AUTHENTIK_SECRET_KEY>"
+  exit 1
+fi
+
+POSTGRESQL_PASSWORD=$1
+AUTHENTIK_SECRET_KEY=$2
+
+helm upgrade \
+   --install authentik authentik/authentik \
+   -f values.yml \
+   --namespace authentik \
+   --set authentik.postgresql.password="$POSTGRESQL_PASSWORD" \
+   --set authentik.secret_key="$AUTHENTIK_SECRET_KEY" \
+   --set postgresql.auth.password="$POSTGRESQL_PASSWORD"
